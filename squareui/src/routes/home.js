08d502f6fedf94aee604787e35ad962945a21de4
armintalaie <talaiearmin78@gmail.com>
2021-11-12 14:44:39 -0800

    auth URL

--- squareui/src/routes/home.js
+++ squareui/src/routes/home.js
@@ -3,13 +3,24 @@ import { ActionButton } from "../theme";
 import { useState } from "react";
 import { theme } from "../App";
 import * as Constants from "../constants/URL";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 export default function Setup() {
   const [form, setForm] = useState("");
   const [accessToken, setaccessToken] = useState("");
   // const [env, setEnv] = useState("sandbox");
   const [authStatus, setAuthStatus] = useState(false);
-
+  const scope = [
+    "CUSTOMERS_WRITE",
+    "CUSTOMERS_READ",
+    "MERCHANT_PROFILE_READ",
+    "MERCHANT_PROFILE_WRITE",
+    "LOYALTY_WRITE",
+    "LOYALTY_READ",
+  ];
+  const authURL =
+    "https://connect.squareup.com/oauth2/authorize?client_id=sq0idp-gmMyNcJXp6Zhwkc342U_6Q&scope=" +
+    scope.join("+") +
+    "&session=False&state=82201dd8d83d23cc8a48caf52b";
   const navigate = useNavigate();
 
   const routeChange = () => {
@@ -19,11 +30,11 @@ export default function Setup() {
 
   async function authorize() {
     // TODO: send request
-    if (!authStatus) {
-      routeChange();
-    } else {
-      alert("something went wrong try again plase");
-    }
+    // if (!authStatus) {
+    //   routeChange();
+    // } else {
+    //   alert("something went wrong try again plase");
+    // }
   }
 
   //   async function storeInfo() {
@@ -38,71 +49,66 @@ export default function Setup() {
   return (
     <div>
       <Container maxWidth="lg" className={theme.root}>
-        <Typography variant="h1">Square Partners Program</Typography>
-
-        <h4>{form}</h4>
+        <Typography variant="h1"> Square Partners Program </Typography>
+        <h4> {form} </h4>{" "}
         <Container maxWidth="md">
           <Paper elevation={1}>
             <Box m={3} p={3} pb={5} pt={5} textAlign={"left"}>
               <Typography variant="h5">
                 Partner with other Square stores to take your loyalty programs
-                to a new level
-              </Typography>
+                to a new level{" "}
+              </Typography>{" "}
               <Typography variant="h5">
                 Give your loyal customers more options by expanding your loyalty
-                program to include more partenring stores
-              </Typography>
-            </Box>
+                program to include more partenring stores{" "}
+              </Typography>{" "}
+            </Box>{" "}
           </Paper>
-
           <Stack direction="row">
             <Paper elevation={1} sx={{ m: 4, p: 3, ml: 0, bgcolor: "#F8F4DD" }}>
               <Box m={3} ml={0} mr={0} p={1} textAlign={"left"}>
-                <Typography variant="h3">How it works</Typography>
-
+                <Typography variant="h3"> How it works </Typography>
                 <Typography variant="h5">
                   {" "}
                   Partner with other Square stores to take your loyalty programs
-                  to a new level
-                </Typography>
+                  to a new level{" "}
+                </Typography>{" "}
                 <Typography variant="h5">
                   {" "}
                   Give your loyal customers more options by expanding your
-                  loyalty program to include more partenring stores
-                </Typography>
-              </Box>
-            </Paper>
+                  loyalty program to include more partenring stores{" "}
+                </Typography>{" "}
+              </Box>{" "}
+            </Paper>{" "}
             <Paper elevation={1} sx={{ m: 4, p: 3, mr: 0, bgcolor: "#F8F4DD" }}>
               <Box m={3} ml={0} mr={0} p={1} textAlign={"left"}>
-                <Typography variant="h3">How it works</Typography>
-
+                <Typography variant="h3"> How it works </Typography>
                 <Typography variant="h5">
                   {" "}
                   Partner with other Square stores to take your loyalty programs
-                  to a new level
-                </Typography>
+                  to a new level{" "}
+                </Typography>{" "}
                 <Typography variant="h5">
                   {" "}
                   Give your loyal customers more options by expanding your
-                  loyalty program to include more partenring stores
-                </Typography>
-              </Box>
-            </Paper>
-          </Stack>
+                  loyalty program to include more partenring stores{" "}
+                </Typography>{" "}
+              </Box>{" "}
+            </Paper>{" "}
+          </Stack>{" "}
         </Container>
-
-        <ActionButton
-          variant="contained"
-          size="large"
-          color="secondary"
-          onClick={() => {
-            authorize();
-          }}
-          sx={{ p: 2, pl: 4, pr: 4, fontSize: 18, m: 4 }}
-        >
-          Integrate with Square
-        </ActionButton>
-      </Container>
+        <a href={authURL}>
+          <ActionButton
+            variant="contained"
+            size="large"
+            color="secondary"
+            onClick={() => {}}
+            sx={{ p: 2, pl: 4, pr: 4, fontSize: 18, m: 4 }}
+          >
+            Integrate with Square{" "}
+          </ActionButton>{" "}
+        </a>
+      </Container>{" "}
     </div>
   );
 }
